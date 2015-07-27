1aaaa5b1ced14d9dba428c2bcfd025f744a2dbaa
Jack <jack.wilsdon2@gmail.com>
2015-07-27 22:05:50 +0100

    Improve code design

--- calculatorapp.rb
+++ calculatorapp.rb
@@ -2,33 +2,32 @@ require 'sinatra/base'
 require 'json'
 
 class CalculatorApp < Sinatra::Base
-  def initialize(app, path = nil)
-    super app
+  include Logging
 
-    @calculator_loader = CalculatorLoader.new(path)
+  ERRORS = {
+    invalid_request: { error: 400, message: "Invalid request" },
+    not_found: { error: 404, message: "Not found" },
+    server_error: { error: 500, message: "Server error" }
+  }
+
+  def self.setup(path = nil, auto_load = true)
+    return self if defined? @loader
+
+    @loader = CalculatorLoader.new path
+
+    self
   end
 
-  def handle_request(endpoint, data)
+  def self.handle_request(endpoint, data)
 
-  end
-
-  def invalid_request_response
-    { error: 400 }
-  end
-
-  def not_found_response
-    { error: 404 }
-  end
-
-  def server_error_response
-    { error: 500 }
   end
 
   post '/:endpoint' do
-    endpoint = @calculator_loader.get_calculator params[:endpoint]
+    p self.methods.sort
+    endpoint = @@loader.get_calculator params[:endpoint]
 
     if endpoint.nil?
-      response = not_found_response
+      response = ERRORS[:not_found]
     else
       begin
         data = JSON.parse request.body.read
@@ -37,13 +36,14 @@ class CalculatorApp < Sinatra::Base
           response = handle_request(endpoint, data)
 
           if response.nil?
-            response = server_error_response
+
+            response = ERRORS[:server_error]
           end
         rescue
-          response = server_error_response
+          response = ERRORS[:server_error]
         end
       rescue
-        response = invalid_request_response
+        response = ERRORS[:invalid_request]
       end
     end
 
