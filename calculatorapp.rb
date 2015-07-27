25bc13fd77d7e7e6584a2adcf069810ac8f9dcd1
Jack <jack.wilsdon2@gmail.com>
2015-07-27 15:22:58 +0100

    Add core application

---
+++ calculatorapp.rb
@@ -0,0 +1,48 @@
+require 'sinatra/base'
+require 'json'
+
+class CalculatorApp < Sinatra::Base
+    Calculators.setup
+
+    def handle_request(endpoint, data)
+
+    end
+
+    def invalid_request_response
+        { error: 400 }
+    end
+
+    def not_found_response
+        { error: 404 }
+    end
+
+    def server_error_response
+        { error: 500 }
+    end
+
+    post '/:endpoint' do
+        endpoint = Calculators.get_endpoint params[:endpoint]
+
+        if endpoint.nil?
+            response = notfound_response
+        else
+            begin
+                data = JSON.parse request.body.read
+
+                begin
+                    response = handle_request(endpoint, data)
+
+                    if response.nil?
+                        response = server_error_response
+                    end
+                rescue
+                    response = server_error_response
+                end
+            rescue
+                response = invalid_request_response
+            end
+        end
+
+        response.to_json
+    end
+end
---
+++ server.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require './calculator.rb'
+require './calculatorapp.rb'
+
+CalculatorApp.run!
