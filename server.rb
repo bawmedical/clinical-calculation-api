e504ce5d6e654fe99d2a63726b6ffcbac1eda1a6
Jack <jack.wilsdon2@gmail.com>
2015-07-28 00:05:04 +0100

    Disable WEBrick access log

--- server.rb
+++ server.rb
@@ -1,11 +1,14 @@
 require 'rubygems'
 require 'bundler/setup'
 
-
 require './logging.rb'
 
 require './calculator.rb'
 require './calculatorloader.rb'
 require './calculatorapp.rb'
+
+CalculatorApp.configure do |app|
+  app.set :server_settings, { AccessLog: [] }
+end
 
 CalculatorApp.setup.run!
