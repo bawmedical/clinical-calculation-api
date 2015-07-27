dbf4d151cfb135c8324844c4897a0573191811d5
Jack <jack.wilsdon2@gmail.com>
2015-07-27 16:48:47 +0100

    Add calculator loader

---
+++ calculatorloader.rb
@@ -0,0 +1,27 @@
+class CalculatorLoader
+  alias_method :reload, :load
+
+  CALCULATOR_SUFFIX = '.rb'
+
+  def initialize(path, auto_load = true)
+    @calculators = []
+    @path = path || './calculators'
+
+    load_calculators if auto_load
+  end
+
+  def get_calculators
+    ObjectSpace.each_object(Class).select { |clazz| clazz < Calculator }
+  end
+
+  def load_calculators
+    Dir[File.join(@path, '*' + CALCULATOR_SUFFIX)].map { |name| load name }
+    @calculators = get_calculators
+
+    self
+  end
+
+  def get_calculator(name)
+    get_calculators.find { |calculator| calculator.get_endpoint == name }
+  end
+end
