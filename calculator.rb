a23c62aacbfaef7fb7b3f6dbd16f24f9cf6ee1e2
Jack <jack.wilsdon2@gmail.com>
2015-07-27 16:49:50 +0100

    Remove unused calculators module

--- calculator.rb
+++ calculator.rb
@@ -12,27 +12,3 @@ class Calculator
     @endpoint = name.downcase
   end
 end
-
-module Calculators
-  def self.instances
-    @@instances
-  end
-
-  def self.all
-    ObjectSpace.each_object(Class).select { |clazz| clazz < Calculator }
-  end
-
-  def self.setup
-    Dir['./calculators/*.rb'].each { |file| require file }
-
-    classes = all
-    duplicate_endpoints = classes - classes.uniq { |clazz| clazz.get_endpoint }
-    duplicate_endpoints.each { |clazz| raise "Duplicate endpoint `#{clazz.get_endpoint}' in class `#{clazz.name}'" }
-
-    @@instances = classes.map { |clazz| clazz.new }
-  end
-
-  def self.get_endpoint(name)
-    instances.find { |endpoint| endpoint.class.get_endpoint === name }
-  end
-end
