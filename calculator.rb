7b5227d7cf0cfff5e4e283718050cd81d54a28c8
Jack <jack.wilsdon2@gmail.com>
2015-07-28 11:23:22 +0100

    Make calculator static and add argument types

--- calculator.rb
+++ calculator.rb
@@ -9,17 +9,33 @@ class Calculator
     @endpoint || self.name.downcase
   end
 
+  def self.get_arg_type(name)
+    return nil if @types.nil?
+
+    @types[name] || :number
+  end
+
   def self.get_all_args
-    self.instance_method(:calculate).parameters.map { |arg| arg[1] }
+    self.method(:calculate).parameters.map { |arg| arg[1] }
   end
 
   def self.get_required_args
-    required = self.instance_method(:calculate).parameters.select { |arg| arg[0] == :keyreq }
+    required = self.method(:calculate).parameters.select { |arg| arg[0] == :keyreq }
     required.map { |arg| arg[1] }
   end
 
   protected
   def self.endpoint(name)
     @endpoint = name.downcase
+  end
+
+  def self.arg_type(name, type)
+    @types = {} if @types.nil?
+
+    if name.kind_of? Array
+      name.each { |n| @types[n] = type }
+    else
+      @types[name] = type
+    end
   end
 end
