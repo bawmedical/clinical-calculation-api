0aa153ebb10a8f26a69b11b83f59a929df6b823a
Jack <jack.wilsdon2@gmail.com>
2015-07-28 21:11:55 +0100

    Only execute setup if the file was run on the command line

--- server.rb
+++ server.rb
@@ -8,13 +8,15 @@ require './lib/calculatorapp.rb'
 require './lib/calculatorloader.rb'
 require './lib/calculatorrouter.rb'
 
-CalculatorApp.configure do |app|
-  app.set :server_settings, { AccessLog: [] }
+if __FILE__ == $0
+  CalculatorApp.configure do |app|
+    app.set :server_settings, { AccessLog: [] }
+  end
+
+  loader = CalculatorLoader.new
+  router = CalculatorRouter.new loader
+
+  loader.load_calculators './calculators'
+
+  CalculatorApp.setup(router).run!
 end
-
-loader = CalculatorLoader.new
-router = CalculatorRouter.new loader
-
-loader.load_calculators './calculators'
-
-CalculatorApp.setup(router).run!
