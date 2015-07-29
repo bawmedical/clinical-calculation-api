e275bc8e059828d40d435549efbf463fbe323f99
Jack <jack.wilsdon2@gmail.com>
2015-07-29 12:34:13 +0100

    Add select methods to symbolize_keys

--- lib/ext/hash.rb
+++ lib/ext/hash.rb
@@ -1,25 +1,40 @@
 class Hash
-  def symbolize_keys
+  def symbolize_keys_select(&block)
     result = {}
 
     self.keys.each do |key|
-      symkey = key.to_sym
+      if !key.respond_to? :to_sym
+        result[key] = self[key]
+        next
+      end
 
-      if !key.is_a? Symbol
-        result[symkey] = self[key]
+      use_key = key.to_sym
+
+      if !block.call key, self[key]
+        use_key = key
       end
 
       if self[key].is_a? Hash
-        result[symkey] = self[key].symbolize_keys
+        result[use_key] = self[key].symbolize_keys_select &block
+      else
+        result[use_key] = self[key]
       end
     end
 
     result
   end
 
-  def symbolize_keys!
+  def symbolize_keys
+    symbolize_keys_select { true }
+  end
+
+  def symbolize_keys_select!(&block)
     self.keys.each do |key|
+      next if !key.respond_to? :to_sym
+
       symkey = key.to_sym
+
+      next if !block.call key, self[key]
 
       if !key.is_a? Symbol
         self[symkey] = self[key]
@@ -27,10 +42,14 @@ class Hash
       end
 
       if self[symkey].is_a? Hash
-        self[symkey].symbolize_keys!
+        self[symkey].symbolize_keys_select! &block
       end
     end
 
     self
+  end
+
+  def symbolize_keys!
+    symbolize_keys_select { true }
   end
 end
