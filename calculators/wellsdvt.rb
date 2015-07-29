a868be405e85b6bd0897e7634e40cdbb7a93cbfb
Niam Patel <naz1997@googlemail.com>
2015-07-29 13:36:56 +0100

    Make wellsdvt calculator variables more verbose

--- calculators/wellsdvt.rb
+++ calculators/wellsdvt.rb
@@ -16,16 +16,16 @@ execute do
   # Bedridden recently >= 3 days or maj surgery within 12 weeks
   # Calf swelling > 3 cm
 
-  act_canc = get_bool("act_canc")
-  bedridden_maj_surgery = get_bool("bedridden_maj_surgery")
+  active_cancer = get_bool("act_canc")
+  bedridden_major_surgery = get_bool("bedridden_maj_surgery")
   calf_swollen = get_bool("calf_swollen")
-  collat_veins = get_bool("collat_veins")
+  collateral_veins = get_bool("collat_veins")
   full_swelling = get_bool("full_swelling")
-  local_tender = get_bool("local_tender")
+  local_tenderness = get_bool("local_tender")
   pitting_edema = get_bool("pitting_edema")
-  immobilized = get_bool("immobilized")
-  prev_dvt = get_bool("prev_dvt")
-  alt_diagnosis = get_bool("alt_diagnosis")
+  immobilized_leg = get_bool("immobilized")
+  previous_dvt = get_bool("prev_dvt")
+  alternate_diagnosis = get_bool("alt_diagnosis")
 
   #raise InvalidRequestError, "act_canc must be a boolean (true / false)",if !field_act_canc.to_bool
   #raise InvalidRequestError, "bedridden_maj_surgery must be a boolean (true / false)" if !field_bedridden_maj_surgery.to_bool
@@ -39,15 +39,15 @@ execute do
   #raise InvalidRequestError, "alt_diagnosis must be a boolean (true / false)" if !field_alt_diagnosis.to_bool
 
   score = 0
-  score += 1 if act_canc
-  score += 1 if bedridden_maj_surgery
+  score += 1 if active_cancer
+  score += 1 if bedridden_major_surgery
   score += 1 if calf_swollen
-  score += 1 if collat_veins
+  score += 1 if collateral_veins
   score += 1 if full_swelling
-  score += 1 if local_tender
+  score += 1 if local_tenderness
   score += 1 if pitting_edema
-  score += 1 if immobilized
-  score -= 2 if alt_diagnosis
+  score += 1 if immobilized_leg
+  score -= 2 if alternate_diagnosis
 
   return score
 end
