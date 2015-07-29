481866e9c9ee4e07a0f9e0282216e5c6c3556d85
Jack <jack.wilsdon2@gmail.com>
2015-07-29 12:41:04 +0100

    Improve centile calculator code

--- calculators/centile/lib/centile.rb
+++ calculators/centile/lib/centile.rb
@@ -1,57 +1,62 @@
-require 'json'
-require 'distribution'
+require "distribution"
 
-lms_hash = JSON.parse(File.read('data/lmsdata.json'))
+class CentileCalculator
+  module Sex
+    MALE = 0
+    FEMALE = 1
+  end
 
-class Centile
+  def initialize(lms_hash)
+    @lms_hash = lms_hash.symbolize_keys_select { |k, v| !k.is_integer? }
+  end
 
-      # age in months; height in cm, weight in kg!
-    def get_height_centile(sex, age_mth, height_cm)
-      if sex=="Male"
-        return height_centile = lms_to_centile(height_cm, lms_hash['male']['height'][age_mth])
-      elsif sex=="Female"
-        return height_centile = lms_to_centile(height_cm, lms_hash['female']['height'][age_mth])
+  def get_height_centile(sex, age, measurement)
+    get_centile sex, age, measurement, :height
+  end
+
+  def get_weight_centile(sex, age, measurement)
+    get_centile sex, age, measurement, :weight
+  end
+
+  def get_bmi_centile(sex, age, measurement)
+    get_centile sex, age, measurement, :bmi
+  end
+
+  private
+
+  def get_centile(sex, age, measurement, type)
+    case sex
+      when Sex::MALE
+        sex_sym = :male
+      when Sex::FEMALE
+        sex_sym = :female
       else
-        #raise error
-      end
-    end
-  
-    def get_weight_centile(sex, age_mth, weight_kg)
-      if sex=="Male"
-        return weight_centile = lms_to_centile(weight, lms_hash['male']['weight'][age_mth])
-      elsif sex=="Female"
-        return weight_centile = lms_to_centile(weight, lms_hash['female']['weight'][age_mth])
-      else
-        #raise error
-      end
-    end
-      
-    def get_bmi_centile(sex, age_mth, bmi)
-      if sex=="Male"
-        return bmi_centile = lms_to_centile(bmi, lms_hash['male']['bmi'][age_mth])
-      elsif sex=="Female"
-        return bmi_centile = lms_to_centile(bmi, lms_hash['female']['bmi'][age_mth])
-      else
-        #raise error
-      end
+        return nil
     end
 
-    def lms_to_centile(x, lms)
-      # formulae taken from http://www.cdc.gov/growthcharts/percentile_data_files.htm
-      # x is the measurement under consideration
-      # returns the percentile as a number from 0 to 100
-      l = lms[0]
-      m = lms[1]
-      s = lms[2]
-      if l==0
-        z = log(x/m)/s
-      else
-        z = (((x/m)**l)-1)/(l*s)
-        centile = 100*Distribution::Normal::cdf(z)
-      end
-      return centile
+    types = @lms_hash[sex_sym]
+    return nil if !types.include? type
+
+    type = types[type]
+    return nil if !type.include? age.to_s
+
+    lms_to_centile measurement, type[age.to_s]
+  end
+
+  def lms_to_centile(x, lms)
+    # formulae taken from http://www.cdc.gov/growthcharts/percentile_data_files.htm
+    # x is the measurement under consideration
+    # returns the percentile as a number from 0 to 100
+    l = lms[:l]
+    m = lms[:m]
+    s = lms[:s]
+
+    if l == 0
+      z = log(x / m) / s
+    else
+      z = (((x / m) ** l) - 1) / (l * s)
     end
 
+    Distribution::Normal::cdf(z) * 100
+  end
 end
-    
-puts lms_to_centile(51.04, [1, 51.04, 0.0391])
