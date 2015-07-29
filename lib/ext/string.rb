01702df9ab639b376606c28da8d3f1196c09fb9a
Jack <jack.wilsdon2@gmail.com>
2015-07-29 15:19:16 +0100

    Remove is_number? alias

--- lib/ext/string.rb
+++ lib/ext/string.rb
@@ -1,4 +1,10 @@
 class String
+  def to_bool
+    return true   if self == true   || self =~ (/(true|t|yes|y|1)$/i)
+    return false  if self == false  || self.empty? || self =~ (/(false|f|no|n|0)$/i)
+    raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
+  end
+
   def is_integer?
     begin
       Integer(self).is_a? Integer
@@ -14,12 +20,4 @@ class String
       false
     end
   end
-
-  def to_bool
-    return true   if self == true   || self =~ (/(true|t|yes|y|1)$/i)
-    return false  if self == false  || self.empty? || self =~ (/(false|f|no|n|0)$/i)
-    raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
-  end
-
-  alias_method :is_number?, :is_float?
 end
