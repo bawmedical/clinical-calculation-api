ed1551177ae85eca22351f2447666231010c168d
Jack <jack.wilsdon2@gmail.com>
2015-07-29 16:06:36 +0100

    Convert field data to float for bmi calculator

--- calculators/bmi.rb
+++ calculators/bmi.rb
@@ -1,8 +1,14 @@
 name :bmi
 
 execute do
-	raise FieldError.new("weight", "weight must be greater than zero") if field_weight <= 0
-	raise FieldError.new("height", "height must be greater than zero") if field_height <= 0
+	raise FieldError.new("weight", "weight must be a number") if !field_weight.is_float?
+	raise FieldError.new("height", "height must be a number") if !field_height.is_float?
 
-	field_weight / (field_height ** 2)
+	weight = field_weight.to_f
+	height = field_height.to_f
+
+	raise FieldError.new("weight", "weight must be greater than zero") if weight <= 0
+	raise FieldError.new("height", "height must be greater than zero") if height <= 0
+
+	weight / (height ** 2)
 end
