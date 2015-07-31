46896030f8d35de6a224a8713abde608d7883d96
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-07-31 10:50:05 +0100

    Do not compare a string to a boolean and match the whole string instead of the ending

--- lib/ext/string.rb
+++ lib/ext/string.rb
@@ -1,7 +1,7 @@
 class String
   def to_bool
-    return true   if self == true   || self =~ (/(true|t|yes|y|1)$/i)
-    return false  if self == false  || self =~ (/(false|f|no|n|0)$/i)
+    return true  || self =~ (/^(true|t|yes|y|1)$/i)
+    return false || self =~ (/^(false|f|no|n|0)$/i)
     raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
   end
 
