a04865467635a260113e66ee2b97fa60a6bc308a
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-03 00:25:47 +0100

    Improve centile code

--- calculators/centile/centile.rb
+++ calculators/centile/centile.rb
@@ -4,35 +4,38 @@ require 'json'
 name :centile
 require_helpers :get_field_as_float, :get_field_as_integer, :months_between, :get_field_as_sex
 
-require_relative "./lib/centilecalculator.rb"
+require_relative './lib/centilecalculator.rb'
 
-centile_data_path = File.expand_path("./data/lmsdata.json", File.dirname(__FILE__))
+centile_data_path = File.expand_path('./data/lmsdata.json', File.dirname(__FILE__))
 
-if !File.exists? centile_data_path
-  logger.error "Missing lmsdata.json!"
+unless File.exist? centile_data_path
+  logger.error 'Missing lmsdata.json!'
   return
 end
 
-centile_calculator = CentileCalculator.new JSON.parse (File.read (centile_data_path))
+centile_calculator = CentileCalculator.new JSON.parse(File.read(centile_data_path))
 
 execute do
   weight = get_field_as_float :weight_in_kg
   height = get_field_as_float :height_in_m
 
-  raise FieldError.new("weight", "must be greater than zero") if weight <= 0
-  raise FieldError.new("height", "must be greater than zero") if height <= 0
+  fail FieldError.new('weight', 'must be greater than zero') if weight <= 0
+  fail FieldError.new('height', 'must be greater than zero') if height <= 0
 
   day_of_birth = get_field_as_integer :day_of_birth
   month_of_birth = get_field_as_integer :month_of_birth
   year_of_birth = get_field_as_integer :year_of_birth
 
+  valid_date = Date.valid_date? year_of_birth, month_of_birth, day_of_birth
+
+  fail InvalidRequestError, 'invalid date of birth' unless valid_date
+
   today = Date.today
 
-  raise InvalidRequestError.new("invalid date of birth") unless Date.valid_date? year_of_birth, month_of_birth, day_of_birth
-  raise FieldError.new("year_of_birth", "must be less than #{today.year + 1}") if year_of_birth > today.year
+  fail FieldError.new('year_of_birth', "must be less than #{today.year + 1}") if year_of_birth > today.year
 
   sex = get_field_as_sex :sex
-  bmi = weight / (height ** 2)
+  bmi = weight / (height**2)
 
   date_of_birth = Date.new year_of_birth, month_of_birth, day_of_birth
   age_in_months = months_between date_of_birth, today
