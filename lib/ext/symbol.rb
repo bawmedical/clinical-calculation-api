ad29d6e893adbbbf868f669c6a85f55854274959
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-03 00:10:10 +0100

    Remove unnecessary self usage

--- lib/ext/symbol.rb
+++ lib/ext/symbol.rb
@@ -1,17 +1,17 @@
 class Symbol
   def +(other)
-    (self.to_s + other.to_s).to_sym
+    (to_s + other.to_s).to_sym
   end
 
   def start_with?(value)
-    self.to_s.start_with? value.to_s
+    to_s.start_with? value.to_s
   end
 
   def sub(pattern, replacement)
-    self.to_s.sub(pattern, replacement.to_s).to_sym
+    to_s.sub(pattern, replacement.to_s).to_sym
   end
 
   def include?(substr)
-    self.to_s.include? substr.to_s
+    to_s.include? substr.to_s
   end
 end
