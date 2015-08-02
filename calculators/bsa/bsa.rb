cca8dc13cc58fd49fe92121f950c18ea1fe85684
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-03 00:18:40 +0100

    Remove redundant field name from error

--- calculators/bsa/bsa.rb
+++ calculators/bsa/bsa.rb
@@ -5,8 +5,8 @@ execute do
   weight = get_field_as_float :weight_in_kg
   height = get_field_as_float :height_in_m
 
-  raise FieldError.new('weight', 'weight must be greater than zero') if weight <= 0
-  raise FieldError.new('height', 'height must be greater than zero') if height <= 0
+  raise FieldError.new('weight', 'must be greater than zero') if weight <= 0
+  raise FieldError.new('height', 'must be greater than zero') if height <= 0
 
   {
     value: ((weight * (height * 100)) / 3600) ** 0.5,
