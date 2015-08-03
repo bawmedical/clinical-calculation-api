f8dc387efbd72362c46b89389d9aae086a2bdc08
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-03 22:51:48 +0100

    Add basic comments to bmi and bsa calculators

--- calculators/bmi/bmi.rb
+++ calculators/bmi/bmi.rb
@@ -1,10 +1,14 @@
 name :bmi
 require_helpers :get_field_as_float
 
+# Define calculation method
 execute do
+
+  # Retrieve fields from request
   weight = get_field_as_float :weight_in_kg
   height = get_field_as_float :height_in_m
 
+  # Ensure fields are valid
   fail FieldError.new('weight', 'must be greater than zero') if weight <= 0
   fail FieldError.new('height', 'must be greater than zero') if height <= 0
 
