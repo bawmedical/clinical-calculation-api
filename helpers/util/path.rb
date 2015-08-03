53675859ef2a4c8a395fc8f53fa836ac7c6bf7c5
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-03 22:12:25 +0100

    Rename path to file helper and add read methods

--- helpers/util/path.rb
+++
@@ -1,9 +0,0 @@
-def resolve_path(_context, filename, file, required = false)
-  path = File.expand_path(filename, File.dirname(file))
-
-  fail ServerError, "missing #{filename}" if required && !File.exist?(path)
-
-  path
-end
-
-add_helper_method method(:resolve_path)
