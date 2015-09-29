3fdfee81e9d56314c7f52a04f368164ef95cc9ff
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-09-29 11:26:19 +0100

    Use new score helper to calculate cha2ds2 score

--- calculators/cha2ds2/cha2ds2.rb
+++ calculators/cha2ds2/cha2ds2.rb
@@ -1,45 +1,31 @@
 class Cha2Ds2Calculator < Calculator
-  SCORING = [
-    # Add 1 point if they are 65-74
-    { points: 1, field: :age, cause: proc { |field| (65..74).include? field } },
+  SCORING = {
+    age: [
+      # Add 1 point if they are 65-74
+      { points: 1, cause: proc { |field| (65..74).include? field } },
 
-    # Add 2 points if they are 75 or older
-    { points: 2, field: :age, cause: proc { |field| field >= 75 } },
+      # Add 2 points if they are 75 or older
+      { points: 2, cause: proc { |field| field >= 75 } }
+    ],
 
     # Add 1 point if they are female
-    { points: 1, field: :sex, cause: :female },
+    sex: { points: 1, cause: :female },
 
     # Add 1 point if they have a history of congestive heart failure
-    { points: 1, field: :congestive_heart_failure_history, cause: true },
+    congestive_heart_failure_history: { points: 1 },
 
     # Add 1 point if they have a history of hypertension
-    { points: 1, field: :hypertension_history, cause: true },
+    hypertension_history: { points: 1 },
 
     # Add 2 points if they have a history of Strokes, TIA or Thromboembolism
-    { points: 2, field: :stroke_history, cause: true },
+    stroke_history: { points: 2 },
 
     # Add 1 point if they have a history of vascular disease
-    { points: 1, field: :vascular_disease_history, cause: true },
+    vascular_disease_history: { points: 1 },
 
     # Add 1 point if they have diabetes
-    { points: 1, field: :diabetes, cause: true }
-  ]
-
-  def get_score(fields)
-    score = 0
-
-    SCORING.each do |item|
-      value = fields[item[:field]]
-
-      if item[:cause].respond_to? :call
-        score += item[:points] if item[:cause].call value
-      else
-        score += item[:points] if item[:cause] == value
-      end
-    end
-
-    score
-  end
+    diabetes: { points: 1 }
+  }
 
   def cha2ds2(_fields, helpers)
     fields = {
@@ -52,7 +38,7 @@ class Cha2Ds2Calculator < Calculator
       diabetes: helpers.get_field_as_bool(:diabetes)
     }
 
-    helpers.generate_response get_score(fields)
+    helpers.generate_response helpers.calculate_score(SCORING, fields)
   end
 
   endpoint :cha2ds2
