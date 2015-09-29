c66e330fa18eb5857a20ff3f3f31372f73ae1a2a
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-09-29 10:59:01 +0100

    Add comments to describe scoring rules

--- calculators/cha2ds2/cha2ds2.rb
+++ calculators/cha2ds2/cha2ds2.rb
@@ -1,12 +1,27 @@
 class Cha2Ds2Calculator < Calculator
   SCORING = [
+    # Add 1 point if they are 65-74
     { points: 1, field: :age, cause: proc { |field| (65..74).include? field } },
+
+    # Add 2 points if they are 75 or older
     { points: 2, field: :age, cause: proc { |field| field >= 75 } },
+
+    # Add 1 point if they are female
     { points: 1, field: :sex, cause: :female },
+
+    # Add 1 point if they have a history of congestive heart failure
     { points: 1, field: :congestive_heart_failure_history, cause: true },
+
+    # Add 1 point if they have a history of hypertension
     { points: 1, field: :hypertension_history, cause: true },
+
+    # Add 2 points if they have a history of Strokes, TIA or Thromboembolism
     { points: 2, field: :stroke_history, cause: true },
+
+    # Add 1 point if they have a history of vascular disease
     { points: 1, field: :vascular_disease_history, cause: true },
+
+    # Add 1 point if they have diabetes
     { points: 1, field: :diabetes, cause: true }
   ]
 
